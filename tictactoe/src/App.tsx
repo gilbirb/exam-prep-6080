ef0d3668d7f39bd0081411eb19a39b5a4e35b148
GILBERT SEBASTIAN YOGIANA <z5641572@unsw.edu.au>
2026-04-22 14:33:56 +1000

    set up grid

--- tictactoe/src/App.tsx
+++ tictactoe/src/App.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react"
 
 function App() {
 
+  const [grid, setGrid] = useState(
+    [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]
+  )
+
   return (
-    <div></div>
+    <div className="min-w-max flex flex-col justify-center items-center">
+      {grid.map((box, idx) => {
+        return (
+          <div className="flex">
+            <div className="w-50 h-50 border-black border-2 inline-flex justify-center items-center">{box[0]}</div>
+            <div className="w-50 h-50 border-black border-2 inline-flex justify-center items-center">{box[1]}</div>
+            <div className="w-50 h-50 border-black border-2 inline-flex justify-center items-center">{box[2]}</div>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
